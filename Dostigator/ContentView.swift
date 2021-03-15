d2939ecfca013ef6a11fe43608cba183dc11311b
Maksim.Lamanskiy <lammax2012@gmail.com>
2021-03-15 17:46:05 +0300

    Start commit

--- Dostigator/ContentView.swift
+++ Dostigator/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
     var body: some View {
         List {
             ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                Text("Item at \(item.timestamp!, formatter: itemFormatter) with title \(item.title ?? "")")
             }
             .onDelete(perform: deleteItems)
         }
---
+++ Lib/DB/Item+adds.swift
@@ -0,0 +1,8 @@
+//
+//  Item+adds.swift
+//  Dostigator
+//
+//  Created by Максим Ламанский on 15.03.2021.
+//
+
+import Foundation
